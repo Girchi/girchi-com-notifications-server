96713ac6277f099cbba36953bbd2d07e552941f4
Ilia Nozadze <i_nozadze@cu.edu.ge>
2020-03-16 00:26:17 +0400

    Read access token from cookies.

--- middleware/index.js
+++ middleware/index.js
@@ -2,17 +2,13 @@ const { verifyToken } = require('../utils');
 
 module.exports = {
 	hasToken: (req, res, next) => {
-		const tokenHeader = req.get('Authorization');
-		if (typeof tokenHeader !== 'string')
+		const accessToken = req.cookies['g-u-at'];
+		if (!accessToken)
 			return next(new Error('Token not found'))
 
-		const token = tokenHeader.split(' ')[1];
-		if (!token)
-			return next(new Error('Can\'t parse token'));
-
-		verifyToken(token)
+		verifyToken(accessToken)
 			.then(decoded => {
-				req.uuid = decoded.uuid;
+				req.uuid = decoded.user_id;
 				next()
 			}).catch(error => next(error));
 	}
